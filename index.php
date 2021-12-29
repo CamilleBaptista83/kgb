0ed030ba7fa89990de9634bfc6371a3704db2254
Camille <cambaptista83@gmail.com>
2021-12-29 17:35:50 +0100

    mise en place de la navigation et du titre

--- index.php
+++ index.php
@@ -25,6 +25,12 @@
 
     ?>
 
+    <section>
+
+        <h1 class="text-center">Les Missions</h1>
+
+    </section>
+
     <!-- Bundle JS et Popper Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
