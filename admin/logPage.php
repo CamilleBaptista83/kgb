00f7f6f8a45cf6698139777376c54da1782011a0
Camille <cambaptista83@gmail.com>
2022-01-09 02:21:50 +0100

    login ok

--- admin/logPage.php
+++ admin/logPage.php
@@ -1,9 +1,33 @@
 <?php
-require  $_SERVER['DOCUMENT_ROOT']."/kgb/components/header.php";
-require  $_SERVER['DOCUMENT_ROOT']."/kgb/components/loadClasses.php";
+require  $_SERVER['DOCUMENT_ROOT'] . "/kgb/components/header.php";
+require  $_SERVER['DOCUMENT_ROOT'] . "/kgb/components/loadClasses.php";
+
+$login = false ;
+
+$manager = new AdminManager();
+
+
+if ($_POST) {
+    var_dump($_POST);
+    $manager->login($_POST['email'], $_POST['password']);
+}
 ?>
+
+<div class="container">
+    <form method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
 
 <?php
-require  $_SERVER['DOCUMENT_ROOT'].'/kgb/components/footer.php';
+require  $_SERVER['DOCUMENT_ROOT'] . '/kgb/components/footer.php';
 ?>
--- controlers/AdminManager.php
+++ controlers/AdminManager.php
@@ -35,40 +35,38 @@ class AdminManager
         return $this;
     }
 
-    public function create(Admin $agent)
+    public function create(Admin $admin)
     {
-        $request = $this->pdo->prepare("INSERT INTO dt_admin(admin_id_uuid, first_name, last_name, email, password, creation_date) VALUES (UUID(), :first_name,  :last_name, :email, :password, :creation_date)");
-        $request->bindValue(':identification_code', $agent->getFirst_name(), PDO::PARAM_STR);
-        $request->bindValue(':first_name', $agent->getLast_name(), PDO::PARAM_STR);
-        $request->bindValue(':last_name', $agent->getEmail(), PDO::PARAM_STR);
-        $request->bindValue(':birth_date', $agent->getPassword(), PDO::PARAM_STR);
-        $request->bindValue(':id_country', $agent->getCreation_date(), PDO::PARAM_STR);
+        $request = $this->pdo->prepare("INSERT INTO dt_admin(admin_id_uuid, first_name, last_name, email, password, creation_date) VALUES (UUID(), :first_name,  :last_name, :email, :password, NOW())");
+        $request->bindValue(':identification_code', $admin->getFirst_name(), PDO::PARAM_STR);
+        $request->bindValue(':first_name', $admin->getLast_name(), PDO::PARAM_STR);
+        $request->bindValue(':last_name', $admin->getEmail(), PDO::PARAM_STR);
+        $request->bindValue(':birth_date', password_hash($admin->getPassword(), PASSWORD_BCRYPT));
         $request->execute();
     }
 
-    public function update(Admin $agent)
+    public function update(Admin $admin)
     {
-        $request = $this->pdo->prepare("UPDATE `dt_admin` SET first_name=:first_name,  last_name=:last_name, email=:email, password=:password, creation_date=:creation_date WHERE admin_id_uuid=:admin_id_uuid");
-        $request->bindValue(':identification_code', $agent->getFirst_name(), PDO::PARAM_STR);
-        $request->bindValue(':first_name', $agent->getLast_name(), PDO::PARAM_STR);
-        $request->bindValue(':last_name', $agent->getEmail(), PDO::PARAM_STR);
-        $request->bindValue(':birth_date', $agent->getPassword(), PDO::PARAM_STR);
-        $request->bindValue(':id_country', $agent->getCreation_date(), PDO::PARAM_STR);
-        $request->bindValue(':agent_id_uuid', $agent->getAdmin_id_uuid(), PDO::PARAM_STR);
+        $request = $this->pdo->prepare("UPDATE `dt_admin` SET first_name=:first_name,  last_name=:last_name, email=:email, password=:password WHERE admin_id_uuid=:admin_id_uuid");
+        $request->bindValue(':identification_code', $admin->getFirst_name(), PDO::PARAM_STR);
+        $request->bindValue(':first_name', $admin->getLast_name(), PDO::PARAM_STR);
+        $request->bindValue(':last_name', $admin->getEmail(), PDO::PARAM_STR);
+        $request->bindValue(':birth_date', $admin->getPassword(), PDO::PARAM_STR);
+        $request->bindValue(':admin_id_uuid', $admin->getAdmin_id_uuid(), PDO::PARAM_STR);
         $request->execute();
     }
 
-    public function delete(string $agent_id_uuid)
+    public function delete(string $admin_id_uuid)
     {
         $request = $this->pdo->prepare("DELETE FROM `dt_admin` WHERE admin_id_uuid= :admin_id_uuid ");
-        $request->bindValue(':agent_id_uuid', $agent_id_uuid, PDO::PARAM_STR);
+        $request->bindValue(':admin_id_uuid', $admin_id_uuid, PDO::PARAM_STR);
         $request->execute();
     }
 
-    public function getById(string $agent_id_uuid)
+    public function getById(string $admin_id_uuid)
     {
         $request = $this->pdo->prepare("SELECT * FROM dt_admin WHERE admin_id_uuid= :admin_id_uuid");
-        $request->bindValue(':agent_id_uuid', $agent_id_uuid, PDO::PARAM_STR);
+        $request->bindValue(':admin_id_uuid', $admin_id_uuid, PDO::PARAM_STR);
         $request->execute();
         $data = $request->fetch();
         return new Admin($data);
@@ -77,12 +75,36 @@ class AdminManager
     public function getAll()
     {
         $request = $this->pdo->query("SELECT * FROM dt_admin ORDER BY identification_code ASC");
-        $agents = array();
+        $admins = array();
         while ($datas = $request->fetch(PDO::FETCH_ASSOC)) {
-            $agents[] = new Admin($datas);
+            $admins[] = new Admin($datas);
         }
-        return $agents;
+        return $admins;
     }
 
 
+    public function login(string $email, string $password)
+    {
+        $request = $this->pdo->prepare("SELECT * FROM dt_admin WHERE email= :email");
+        $request->bindValue(':email', $email, PDO::PARAM_STR);
+        if ($request->execute()) {
+            $admin = $request->fetch(PDO::FETCH_ASSOC);
+            if ($admin === false) {
+                // Si aucun utilisateur ne correspond au login entré, on affiche une erreur
+                echo 'Identifiants invalides';
+                $login = true ;
+                var_dump($login);
+
+            } else {
+                // On vérifie le hash du password
+                if (password_verify($password, $admin['password'])) {
+                    echo 'Bienvenue ' . $admin['last_name'];
+                } else {
+                    echo 'Identifiants invalides';
+                }
+            }
+        } else {
+            echo 'Impossible de récupérer l\'utilisateur';
+        }
+    }
 }
