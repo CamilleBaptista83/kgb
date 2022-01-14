34da8f1efa683e6074d70d8ae52ebe55a1f54c3f
Camille Baptista <cambaptista83@gmail.com>
2022-01-14 23:51:31 +0100

    test

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#"><img class="logo" src="/kgb/img/logo_KGB.png" /></a>
+                <a class="navbar-brand" href="#"><img class="logo" src="img/logo_KGB.png" /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -35,10 +35,10 @@
                     <div class="container">
                         <ul class="nav justify-content-end">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/kgb/index.php">Acceuil</a>
+                                <a class="nav-link active" aria-current="page" href="./index.php">Acceuil</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/kgb/admin/logPage.php">Admin</a>
+                                <a class="nav-link" href="./admin/logPage.php">Admin</a>
                             </li>
                         </ul>
                     </div>
