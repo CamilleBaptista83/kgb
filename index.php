8216d92ca5f44f442bca8834306f93f3a0c08e4c
Camille Baptista <cambaptista83@gmail.com>
2022-01-14 22:59:26 +0100

    test

--- index.php
+++ index.php
@@ -8,21 +8,8 @@ require  $_SERVER['DOCUMENT_ROOT'] . "/kgb/components/loadClasses.php";
 <main>
     <?php
 
-    // MISSIONS
-
-    // creation d'un nouvel obj
-    $managerMission = new MissionsManager();
-    // appel de la fonction get all pour récupérer les données
-    $missions = $managerMission->getAll();
+    echo ('hello');
     ?>
-    <div class="container">
-        <h1 class="text-center">Les Missions</h1>
-
-        <?php
-        foreach ($missions as $mission) {
-            require "/kgb/vue/affichage_missions.php";
-        }
-        ?>
     </div>
 
 </main>
