ad912bda282c15b1170423837e79dbc4b874c814
Camille Baptista <cambaptista83@gmail.com>
2022-01-14 22:54:10 +0100

    ok avec le hello, test avec db

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
-echo ('hello');
+session_start();
+
+require  $_SERVER['DOCUMENT_ROOT'] . "/kgb/components/header.php";
+require  $_SERVER['DOCUMENT_ROOT'] . "/kgb/components/loadClasses.php";
+?>
+
+<main>
+    <?php
+
+    // MISSIONS
+
+    // creation d'un nouvel obj
+    $managerMission = new MissionsManager();
+    // appel de la fonction get all pour récupérer les données
+    $missions = $managerMission->getAll();
+    ?>
+    <div class="container">
+        <h1 class="text-center">Les Missions</h1>
+
+        <?php
+        foreach ($missions as $mission) {
+            require "../kgb/vue/affichage_missions.php";
+        }
+        ?>
+    </div>
+
+</main>
+
+<?php
+require  $_SERVER['DOCUMENT_ROOT'] . '/kgb/components/footer.php';
 ?>
